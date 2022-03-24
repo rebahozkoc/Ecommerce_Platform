94354499dcf1f9b64d8d964853cffca333415eb2
Selimgul14 <76742461+Selimgul14@users.noreply.github.com>
2022-03-24 11:48:34 +0300

    Added colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary = const Color(0xFFFF6600);
+  static const Color secondary = const Color(0xFFDDDDDD);
+  static const Color primaryLight = const Color(0xFFff944e);
+}
