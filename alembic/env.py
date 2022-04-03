41737ee7b53e00121c74201f7ebfdcc5a167d69c
Yasin Ughur <yasinyg@code.edu.az>
2022-04-03 15:58:56 +0300

    pytest config is added

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,15 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import alembic
 
 import os, sys
 from dotenv import load_dotenv
+
+from core.config import settings  # noqa
+from sqlalchemy import engine_from_config, create_engine, pool
+from psycopg2 import DatabaseError
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
@@ -27,7 +33,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from db.base import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -37,7 +44,7 @@ target_metadata = None
 # import models
 # target_metadata = models.Base.metadata
 
-def run_migrations_offline():
+def run_migrations_online():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -49,38 +56,45 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    DB_URL = f"{settings.DATABASE_URL}_test" if os.environ.get("TESTING") else str(settings.DATABASE_URL)
 
-    with context.begin_transaction():
-        context.run_migrations()
+    if os.environ.get("TESTING"):
+        # connect to primary db
+        default_engine = create_engine(str(settings.DATABASE_URL), isolation_level="AUTOCOMMIT")
+        # drop testing db if it exists and create a fresh one
+        with default_engine.connect() as default_conn:
+            default_conn.execute(f"DROP DATABASE IF EXISTS {settings.DB_NAME}_test")
+            default_conn.execute(f"CREATE DATABASE {settings.DB_NAME}_test")
+
+    connectable = config.attributes.get("connection", None)
+    config.set_main_option("sqlalchemy.url", DB_URL)
+
+    if connectable is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool,
+        )
+
+    with connectable.connect() as connection:
+        alembic.context.configure(connection=connection, target_metadata=None)
+
+        with alembic.context.begin_transaction():
+            alembic.context.run_migrations()
 
 
-def run_migrations_online():
+def run_migrations_offline():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    if os.environ.get("TESTING"):
+        raise DatabaseError("Running testing migrations offline currently not permitted.")
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    alembic.context.configure(url=str(settings.DATABASE_URL))
 
-        with context.begin_transaction():
-            context.run_migrations()
+    with alembic.context.begin_transaction():
+        alembic.context.run_migrations()
 
 
 if context.is_offline_mode():
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from core.config import settings
 from fastapi import FastAPI
+from api.api_v1.api import api_router as api_router_v1
 
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_NAME, description=settings.APP_DESCRIPTION, docs_url= settings.API_V1_STR + "/docs"
+)
 
+app.include_router(api_router_v1, prefix=settings.API_V1_STR)
 
 @app.get("/")
 def index():
     return {"msg": "Hello World Test"}
-
