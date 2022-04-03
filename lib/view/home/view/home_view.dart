192b2a9f6c77b322eabcd98153f2adc00a617720
furkankyildirim <furkan.k.yildirim@gmail.com>
2022-04-03 21:55:34 +0300

    Starts large product item view

--- lib/view/home/view/home_view.dart
+++ lib/view/home/view/home_view.dart
@@ -3,6 +3,8 @@ import 'package:mobile/core/base/state/base_state.dart';
 import 'package:mobile/core/base/view/base_widget.dart';
 import 'package:mobile/core/extension/string_extension.dart';
 import 'package:mobile/core/init/lang/locale_keys.g.dart';
+import 'package:mobile/core/widgets/customScrollPhysics.dart';
+import 'package:mobile/core/widgets/productItems/large_product.dart';
 import 'package:mobile/locator.dart';
 import 'package:mobile/view/home/viewmodel/home_view_model.dart';
 
@@ -39,7 +41,24 @@ class _HomeViewState extends BaseState<HomeView> {
     );
   }
 
-  Center _body() => Center(
-        child: Text(LocaleKeys.explore.locale),
+  SizedBox _body() => SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 280,
+                child: ListView.builder(
+                  primary: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: const CustomScrollPhysics(itemDimension: 264),
+                  itemCount: 5,
+                  itemBuilder: (context, index) => const LargeProduct(),
+                ),
+              )
+            ],
+          ),
+        ),
       );
 }
