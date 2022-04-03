7011bb661b0d13066dfe545d3dee29061f67872e
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2022-04-04 01:47:49 +0300

    Created sticky header

---
+++ my-app/src/components/theme.js
@@ -0,0 +1,14 @@
+import {createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+      secondary: {
+        main: '#E6920A'
+      },
+      primary: {
+        main: '#FFFFFF'
+    }
+  },
+  });
+
+export default theme;
