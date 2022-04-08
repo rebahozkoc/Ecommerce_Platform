1cb41740754464afeda708ee7a1d8bfb8e5ab444
“ysnghr” <yasinugur.cs@gmail.com>
2022-04-08 19:32:13 +0300

    chmod permission fix

--- alembic/env.py
+++ alembic/env.py
@@ -1,63 +1,38 @@
-from logging.config import fileConfig
+import pathlib
+import sys
+import os
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
 
-from alembic import context
 import alembic
-
-import os, sys
-from dotenv import load_dotenv
-
-from core.config import settings  # noqa
 from sqlalchemy import engine_from_config, create_engine, pool
 from psycopg2 import DatabaseError
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-sys.path.append(BASE_DIR)
+from logging.config import fileConfig
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+# we're appending the app directory to our path here so that we can import config easily
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
-config.set_main_option("sqlalchemy.url", os.environ.get("DATABASE_URL"))
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+from core.config import settings # noqa
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 from db.base import Base
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# Alembic Config object, which provides access to values within the .ini file
+config = alembic.context.config
 
-# import models
-# target_metadata = models.Base.metadata
+# Interpret the config file for logging
+fileConfig(config.config_file_name)
 
-def run_migrations_online():
-    """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+def run_migrations_online() -> None:
+    """
+    Run migrations in 'online' mode
     """
     DB_URL = f"{settings.DATABASE_URL}_test" if os.environ.get("TESTING") else str(settings.DATABASE_URL)
 
+    # handle testing config for migrations
     if os.environ.get("TESTING"):
         # connect to primary db
         default_engine = create_engine(str(settings.DATABASE_URL), isolation_level="AUTOCOMMIT")
@@ -75,19 +50,17 @@ def run_migrations_online():
         )
 
     with connectable.connect() as connection:
-        alembic.context.configure(connection=connection, target_metadata=None)
+        alembic.context.configure(connection=connection, target_metadata=Base.metadata)
 
         with alembic.context.begin_transaction():
             alembic.context.run_migrations()
 
 
-def run_migrations_offline():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+def run_migrations_offline() -> None:
     """
+    Run migrations in 'offline' mode.
+    """
+
     if os.environ.get("TESTING"):
         raise DatabaseError("Running testing migrations offline currently not permitted.")
 
@@ -97,7 +70,7 @@ def run_migrations_offline():
         alembic.context.run_migrations()
 
 
-if context.is_offline_mode():
+if alembic.context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
