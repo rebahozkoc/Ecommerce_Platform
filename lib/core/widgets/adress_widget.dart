4ce3a99a54462585e5b41fc26579a42b3ffa39e8
furkankyildirim <furkan.k.yildirim@gmail.com>
2022-04-25 10:49:45 +0300

    Removes Google Maps installation

--- lib/core/widgets/adress_widget.dart
+++ lib/core/widgets/adress_widget.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AddressWidget extends StatelessWidget {
   final double latitude;
@@ -14,27 +11,9 @@ class AddressWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CameraPosition _position = CameraPosition(
-      target: LatLng(latitude, longitude),
-      zoom: 14.4746,
-    );
 
-    return SizedBox(
+    return const SizedBox(
       height: 250,
-      child: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _position,
-        gestureRecognizers: {
-          Factory<OneSequenceGestureRecognizer>(() => ScaleGestureRecognizer()),
-        },
-        myLocationButtonEnabled: false,
-        compassEnabled: false,
-        zoomControlsEnabled: false,
-        zoomGesturesEnabled: false,
-        myLocationEnabled: false,
-        mapToolbarEnabled: false,
-        
-      ),
     );
   }
 }
