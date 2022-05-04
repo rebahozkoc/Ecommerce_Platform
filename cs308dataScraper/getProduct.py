a0bd2bd5617a7fa6c0c0e56294b5d06cb7aaeb1e
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2022-05-05 01:47:07 +0300

    Get product [data scraping]

--- cs308dataScraper/getProduct.py
+++ cs308dataScraper/getProduct.py
@@ -1,30 +1,29 @@
 import requests
 import random
-productURL = "https://www.vivense.com/lizbon-koltuk-takimi-kaz-ayakli-modeli.html"
+productURL = "https://www.vivense.com/terre-koltuk-takimi-krem-modeli.html"
 productsFileName = "koltuklar.json"
 
 import json
 from bs4 import BeautifulSoup
 
-r = requests.get(productURL)
-result = r.text
-soup = BeautifulSoup(result, "lxml")
+def getProduct(url, fileName, category, subCategory):
+    r = requests.get(url)
+    result = r.text
+    soup = BeautifulSoup(result, "lxml")
 
-title = soup.find(class_="product-title").text
-stock = random.randint(0, 100)
-price = int(soup.find(class_="price new-price").attrs["data-value"])
-description = ""
-warrantyStatus = str(random.randint(1, 5)) + "year(s)"
-distributor = "Voidture Inc."
+    title = soup.find(class_="product-title").text
+    stock = random.randint(0, 100)
+    price = int(soup.find(class_="price new-price").attrs["data-value"])
+    description = ""
+    warrantyStatus = str(random.randint(1, 5)) + "year(s)"
+    distributor = "Voidture Inc."
 
-for i in soup.find(class_="desctab").findAll(name="tr"):
-    description += ": ".join([j.text for j in i])
-    description += "\n"
+    for i in soup.find(class_="desctab").findAll(name="tr"):
+        description += ": ".join([j.text for j in i])
+        description += "\n"
 
-data_file = open(productsFileName, "a", encoding="UTF-8")
+    data_file = open(fileName, "a", encoding="UTF-8")
 
-
-
-data = {'title': title, "price": price, "description": description, "warrantyStatus": warrantyStatus, "distributor": distributor, "stock": stock}
-json.dump(data, data_file, ensure_ascii=False)
-data_file.write(",\n")
+    data = {'title': title, "price": price, "description": description, "warrantyStatus": warrantyStatus, "distributor": distributor, "stock": stock, "category": category, "subCategory": subCategory}
+    json.dump(data, data_file, ensure_ascii=False)
+    data_file.write(",\n")
--- cs308dataScraper/main.py
+++ cs308dataScraper/main.py
@@ -1,21 +1,33 @@
-import requests
-categoryURL = "https://www.vivense.com/koltuk-takimlari-mobilyalari.html?ref=menu_text"
-cateforyFileName = "koltuk.json"
+from time import sleep
 
+import requests
+
+from getProduct import getProduct
+
+categoryURL = "https://www.vivense.com/koltuk-takimlari-mobilyalari.html?ref=menu_text"
+cateforyFileName = ".json"
+categoryName = "Living Room"
+subcategoryName = "Sofa Set"
 import json
 from bs4 import BeautifulSoup
 
 r = requests.get(categoryURL)
 result = r.text
 soup = BeautifulSoup(result, "lxml")
-product_list1 = soup.find(class_="product-list")
 product_list = soup.find_all(class_="product-card product-content parent")
 data_file = open(cateforyFileName, "a", encoding="UTF-8")
 
 for i in product_list:
+    product_name = i.find(class_="product-name").get_text()
+    productUrl = "https://www.vivense.com/" + i.find(class_="product-link").findChild(class_="product-link").attrs["href"]
+    getProduct(productUrl, cateforyFileName, categoryName, subcategoryName)
+    sleep(1)
+    print(product_name)
+    """
     product_name = i.attrs["data-product-name"]
     price = i.attrs["data-product-price"]
     image = i.find(class_="product_img").findChild().attrs["data-main-img"]
     data = {'productName': product_name, "price": price, "image": image}
     json.dump(data, data_file, ensure_ascii=False)
     data_file.write(",\n")
+    """
