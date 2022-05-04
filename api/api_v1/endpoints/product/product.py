c851df2adaf0b2f35a239701aeb9c8a5e4bbd685
Yasin Ughur <yasinugur.cs@gmail.com>
2022-05-04 05:45:39 +0300

    photos of product crud operations

---
+++ api/api_v1/endpoints/product/product.py
@@ -0,0 +1,76 @@
+from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
+from sqlalchemy.orm import Session
+from api import deps
+
+import crud, models, schemas
+from schemas import Response
+from typing import Any
+from utilities.image import ImageUtilities
+
+router = APIRouter()
+
+
+@router.get("/{id}", response_model=Response[schemas.ProductBase])
+def get_product(
+    id: int,
+    db: Session = Depends(deps.get_db),
+) -> Any:
+    """
+    Retrieve product from id
+    """
+    product = crud.product.get(db=db, field="id", value=id)
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": f"Product does not exists"},
+        )
+    return Response(data=product)
+
+
+@router.post("/{id}/photo/add", response_model=Response)
+async def add_photo_to_product(
+    *,
+    id: int,
+    photo: UploadFile = File(...),
+    db: Session = Depends(deps.get_db),
+):
+    """
+    Uploads photo of product.
+    """
+    product = crud.product.get(db=db, field="id", value=id)
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": f"Product does not exists"},
+        )
+
+    added_photo = crud.product.add_photo(db=db, product=product, photo=photo)
+    photo_url = ImageUtilities.get_image_url(added_photo.photo_url)
+    return Response(data=photo_url, message="Uploaded photo successfully")
+
+
+@router.delete("/{id}/photo/{photo_id}/remove", response_model=Response)
+async def remove_photo_to_product(
+    *,
+    id: int,
+    photo_id: int,
+    db: Session = Depends(deps.get_db),
+):
+    """
+    Removes photo of product.
+    """
+    product = crud.product.get(db=db, field="id", value=id)
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": f"Product does not exists"},
+        )
+
+    added_photo = crud.product.remove_photo(db=db, photo_id=photo_id)
+    if not added_photo:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": f"Photo of the product does not exists"},
+        )
+    photo_url = ImageUtilities.get_image_url(added_photo.photo_url)
+    return Response(data=photo_url, message="Deleted photo successfully")
--- crud/crud_product.py
+++ crud/crud_product.py
@@ -1,17 +1,43 @@
 from crud.base import CRUDBase
 from sqlalchemy.orm import Session
 from typing import List
-from models.product import Product
+from models.product import Product, ProductPhoto
 import schemas
-from fastapi import status, HTTPException
+from fastapi import status, HTTPException, UploadFile
+from utilities.image import ImageUtilities
 
-class CRUDProduct(CRUDBase[schemas.ProductBase, schemas.ProductCreate, schemas.ProductUpdate]):
-    def get_comments(self, db: Session, id: int, skip: int, limit: int) -> List[schemas.CommentBase]:
+
+class CRUDProduct(
+    CRUDBase[schemas.ProductBase, schemas.ProductCreate, schemas.ProductUpdate]
+):
+    def get_comments(
+        self, db: Session, id: int, skip: int, limit: int
+    ) -> List[schemas.CommentBase]:
         data = db.query(Product).filter(Product.id == id).first()
         if not data:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                    detail={"message": f"Product does not exists"})
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={"message": f"Product does not exists"},
+            )
         return data.comments.offset(skip).limit(limit).all()
+
+    def add_photo(self, db: Session, product: Product, photo: UploadFile):
+        photo_url = ImageUtilities.save_image(photo, "products")
+        product_photo = ProductPhoto(photo_url=photo_url, product_id=product.id)
+        db.add(product_photo)
+        db.commit()
+        db.refresh(product_photo)
+        return product_photo
+
+    def remove_photo(self, db: Session, photo_id: int):
+        db_obj = db.query(ProductPhoto).filter(ProductPhoto.id == photo_id).first()
+
+        if not db_obj:
+            return None
+        ImageUtilities.remove_image(path=db_obj.photo_url)
+        db.delete(db_obj)
+        db.commit()
+        return db_obj
 
 
 product = CRUDProduct(Product)
