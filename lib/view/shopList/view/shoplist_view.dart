025bb28c6f40e6752e75252c5ed988e321bb061d
Selimgul14 <76742461+Selimgul14@users.noreply.github.com>
2022-05-06 01:20:27 +0300

    Completed shopping cart

--- lib/view/shopList/view/shoplist_view.dart
+++ lib/view/shopList/view/shoplist_view.dart
@@ -3,7 +3,9 @@ import 'package:mobile/core/base/state/base_state.dart';
 import 'package:mobile/core/base/view/base_widget.dart';
 import 'package:mobile/core/extension/string_extension.dart';
 import 'package:mobile/core/init/lang/locale_keys.g.dart';
+import 'package:mobile/core/init/theme/color_theme.dart';
 import 'package:mobile/locator.dart';
+import 'package:mobile/view/product/view/product_view.dart';
 import 'package:mobile/view/shopList/viewmodel/shoplist_view_model.dart';
 import 'package:mobile/core/widgets/productItems/shopping_cart_product.dart';
 
@@ -47,7 +49,68 @@ class _ShopListViewState extends BaseState<ShopListView> {
   }
 
   Center _body() => Center(
-          child: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            ListView.builder(itemCount: 5, itemBuilder: (context, index){
+              return CartProduct();
+            }),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                height: 60,
+                decoration: const BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RichText(
+                              text: const TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: "Total: ",
+                                    style: TextStyle(
+                                      color: AppColors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 16,
+                                    )
+                                  ),
+                                  TextSpan(
+                                    text: "310 TL",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.white,
+                                    )
+                                  )
+                                ]
+                              )
+                          ),
+                          _completeShopping(),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+              ),
+            )
+          ],
+        ),
+      /*child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
@@ -60,5 +123,25 @@ class _ShopListViewState extends BaseState<ShopListView> {
             CartProduct(),
           ],
         ),
-      ));
+      )*/
+  );
+
+  OutlinedButton _completeShopping() => OutlinedButton(
+    onPressed: () => viewModel.navigateToPayment(),
+    child: const Text(
+      "Buy Now",
+      style: TextStyle(
+        color: AppColors.black,
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    style: OutlinedButton.styleFrom(
+        backgroundColor: AppColors.white,
+        primary: AppColors.white,
+        fixedSize: const Size(150, 50),
+        side: const BorderSide(width: 1.0, color: AppColors.white)
+    ),
+  );
+
 }
