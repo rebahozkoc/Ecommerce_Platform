fc47bb1b61ef5cc700f73ba3654a395c970c0c15
gorkem yar <gorkemyar@sabanciuniv.edu>
2022-05-06 22:26:21 +0300

    category page endpoint

--- my-app/src/containers/categoryProduct.js
+++ my-app/src/containers/categoryProduct.js
@@ -13,14 +13,15 @@ import {
   useRecoilValue,
 } from "recoil";
 import { useParams, useLocation } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { getDataWithoutAccess } from "../components/recoils/getterFunctions";
 const cards = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
 ];
 const CategoryProduct = () => {
   const { type } = useParams();
-  console.log(type);
+  //console.log(type);
   const stateParamValue = useLocation();
   const title =
     stateParamValue.state != null
@@ -34,8 +35,23 @@ const CategoryProduct = () => {
 
   const subId =
     stateParamValue.state != null ? stateParamValue.state.subId : null;
-  console.log(subId);
-  console.log(catId);
+
+  const [isLoaded, setLoaded] = useState(false);
+  const [products, setProducts] = useState([]);
+  const lastId = subId || catId;
+
+  useEffect(() => {
+    getDataWithoutAccess(
+      `http://164.92.208.145/api/v1/categories/${lastId}`
+    ).then((res) => {
+      console.log(res.data.products);
+      setProducts(res.data.products);
+      setLoaded(true);
+    });
+  }, []);
+
+  //console.log(subId);
+  //console.log(catId);
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -50,7 +66,11 @@ const CategoryProduct = () => {
       <ThemeProvider theme={themeOptions}>
         <PrimarySearchAppBar></PrimarySearchAppBar>
         <ResponsiveAppBar></ResponsiveAppBar>
-        <CardItemHandler item={cards} title={title}></CardItemHandler>
+        {isLoaded ? (
+          <CardItemHandler item={products} title={title}></CardItemHandler>
+        ) : (
+          <div>Loading...</div>
+        )}
       </ThemeProvider>
       <Box sx={{ m: 2 }} />
       <Footer />
