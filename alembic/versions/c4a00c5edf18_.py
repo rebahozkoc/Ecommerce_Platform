6f35e9ef09169e48078ed64659160fe77c853ed7
Yasin Ughur <yasinugur.cs@gmail.com>
2022-05-07 09:08:05 +0000

    alembic fix from server

---
+++ alembic/versions/c4a00c5edf18_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c4a00c5edf18
+Revises: ac649fb7fc29, 4cb26896f32f
+Create Date: 2022-05-07 09:02:17.162208
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c4a00c5edf18'
+down_revision = ('ac649fb7fc29', '4cb26896f32f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
