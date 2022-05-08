ad0cb565c0398e4d8aaa904979fada2197242621
AlbertDenizLevi <78790996+AlbertDenizLevi@users.noreply.github.com>
2022-05-08 04:26:24 +0300

    tests

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -6,6 +6,8 @@ from fastapi.testclient import TestClient
 
 from core.config import settings
 
+def random_integer() -> int:
+    return random.randint(0, 100)
 
 def random_lower_string() -> str:
     return "".join(random.choices(string.ascii_lowercase, k=32))
