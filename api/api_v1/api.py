8f2ae683b9f22583dcf25cbd6423c9703e80b652
Yasin Ughur <yasinugur.cs@gmail.com>
2022-05-08 16:20:51 +0300

    search functionality

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from api.api_v1.endpoints import users
 from api.api_v1.endpoints.authentication import login, registration
-from api.api_v1.endpoints.product import comment, product, rate_product
+from api.api_v1.endpoints.product import comment, product, rate_product, search
 from api.api_v1.endpoints.category import category
 from api.api_v1.endpoints.category import subcategory
 from api.api_v1.endpoints.user import address
@@ -21,6 +21,7 @@ api_router.include_router(login.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(registration.router, prefix="/auth", tags=["Authentication"])
 
 api_router.include_router(product.router, prefix="/products", tags=["Products"])
+api_router.include_router(search.router, prefix="/products", tags=["Products"])
 api_router.include_router(rate_product.router, prefix="/products", tags=["Products"])
 
 api_router.include_router(comment.router, prefix="/products", tags=["Comments"])
@@ -31,5 +32,3 @@ api_router.include_router(
 api_router.include_router(
     subcategory.router, prefix="/categories", tags=["Categories & Subcategories"]
 )
-
-
