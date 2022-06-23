f2d70f3677bb7ee99e881f2fde63a8e9714d4919
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2022-06-23 11:03:47 +0300

    orderPage updated

--- my-app/src/components/account/order/orderItem.js
+++ my-app/src/components/account/order/orderItem.js
@@ -5,12 +5,18 @@ import PendingActionsTwoToneIcon from "@mui/icons-material/PendingActionsTwoTone
 import LocalShippingTwoToneIcon from "@mui/icons-material/LocalShippingTwoTone";
 import CheckCircleOutlineTwoToneIcon from "@mui/icons-material/CheckCircleOutlineTwoTone";
 import { Link } from "react-router-dom";
+import OrderMiniItem from "./orderMiniItem";
+
 const OrderItem = (props) => {
   const order = props.data;
+  console.log("OrderItem", order);
+  var totalPrice = 0;
+
+
   return (
     <Card
       sx={{
-        minHeight: 100,
+        minHeight: 100 + order.order_details.length * 100,
         display: "block",
         borderRadius: 3,
         border: 1,
@@ -19,83 +25,41 @@ const OrderItem = (props) => {
       }}
     >
       <CardContent sx={{ pt: 3 }}>
-        <Stack direction="row" alignItems="center">
-          
-          <Link
-            to={`/product/${order.product.title}`}
-            underline="none"
-            state={{ id: order.product.id }}
-            style={{
-              textDecoration: "none",
-              color: "black",
-            }}
-          >
+        <Stack direction="column" justifyContent="center">
+          <Stack direction="row" alignItems="center">
             <img
-              src={order.product.photos[0].photo_url}
+              src={order.order_details[0].product.photos[0].photo_url}
               height={64}
               alt={"Voidture not Found"}
             />
-          </Link>
-          <Stack
-            direction="column"
-            alignItems="flex-start"
-            justifyContent="center"
-            ml={2}
-          >
-            <Typography component="div" sx={{ mb: 1 }} fontSize={10}>
-              {order.product.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Quantity: {order.quantity}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Price: {order.product.price}
-            </Typography>
+            <Stack
+              direction="column"
+              alignItems="flex-start"
+              justifyContent="center"
+              ml={2}
+            >
+              <Typography component="div" sx={{ mb: 1 }} fontSize={10}>
+                {order.address.name}
+              </Typography>
+            </Stack>
+
+            <Box flexGrow={1}></Box>
+
+            <Box flexGrow={2}></Box>
           </Stack>
 
-          <Box flexGrow={1}></Box>
+          {order.order_details.map((product, index) => {
+            return <OrderMiniItem data={product} />;
+          })}
+          <Box flexGrow={5}></Box>
 
-          {(() => {
-            //change status to order.order_status
-
-            if (order.order_status === "PROCESSING") {
-              return (
-                <Stack direction="row" gap={1}>
-                  <PendingActionsTwoToneIcon
-                    color="primary"
-                    style={{ fontSize: 25 }}
-                  />
-                  {order.order_status}
-                </Stack>
-              );
-            } else if (order.order_status === "In-transit") {
-              return (
-                <Stack direction="row" gap={1}>
-                  <LocalShippingTwoToneIcon
-                    color="primary"
-                    style={{ fontSize: 25 }}
-                  />
-                  {order.order_status}
-                </Stack>
-              );
-            } else if (order.order_status === "Delivered") {
-              return (
-                <Stack direction="row" gap={1}>
-                  <CheckCircleOutlineTwoToneIcon
-                    color="primary"
-                    style={{ fontSize: 25 }}
-                  />
-                  {order.order_status}
-                </Stack>
-              );
-            }
-          })()}
-
-          <Box flexGrow={2}></Box>
-
-          <Stack direction="row" gap={1}>
+          <Stack direction="row" justifyContent="end" gap={1}>
             <Typography variant="h6" style={{ fontWeight: 600 }}>
-              Order total: {order.quantity * order.product.price} $
+              Order total:{" "}
+              
+              {order.order_details[0].product.price *
+                order.order_details[0].quantity}{" "}
+              $
             </Typography>
           </Stack>
         </Stack>
@@ -105,43 +69,3 @@ const OrderItem = (props) => {
 };
 
 export default OrderItem;
-
-/*
-<div className="order-item">
-            <div className="order-item-header">
-                <div className="order-item-header-left">
-                    <div className="order-item-header-left-order-id">
-                        Order #{order.orderId}
-                    </div>
-                    <div className="order-item-header-left-order-date">
-                        {order.orderDate}
-                    </div>
-                </div>
-                <div className="order-item-header-right">
-                    <div className="order-item-header-right-order-status">
-                        {order.orderStatus}
-                    </div>
-                    <div className="order-item-header-right-order-total">
-                        {order.orderTotal}
-                    </div>
-                </div>
-            </div>
-            <div className="order-item-body">
-                <div className="order-item-body-image">
-                    <img src={order.orderImage} alt="order-image" />
-                </div>
-                <div className="order-item-body-details">
-                    <div className="order-item-body-details-product-name">
-                        Product Name
-                    </div>
-                    <div className="order-item-body-details-product-price">
-                        $100
-                    </div>
-                    <div className="order-item-body-details-product-quantity">
-                        Quantity: 1
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        */
